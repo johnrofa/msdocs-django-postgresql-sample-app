6a6c6cea7999326785b6e3fcb1cec3ae0459e90c
johnrofa <johnrofa@hotmail.com>
2023-04-10 18:10:41 -0500

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -5,8 +5,6 @@ from .settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-#WEBSITE_HOSTNAME = 'msdocs-python-postgres-ate-server.postgres.database.azure.com'
-
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
@@ -34,9 +32,9 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['msdocs-python-postgres-ate-database'],
-        'HOST': conn_str_params['msdocs-python-postgres-ate-server.postgres.database.azure.com'],
-        'USER': conn_str_params['peookipoxv'],
-        'PASSWORD': conn_str_params['J71U165O342301LO$'],
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password'],
     }
 }
